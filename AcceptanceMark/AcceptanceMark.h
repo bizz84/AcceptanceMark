c27a98f20f55fa091393f7544286f6c6c48532cd
Andrea Bizzotto <bizz84@gmail.com>
2016-08-08 12:59:43 +0100

    Initial Commit

---
+++ AcceptanceMark/AcceptanceMark.h
@@ -0,0 +1,19 @@
+//
+//  AcceptanceMark.h
+//  AcceptanceMark
+//
+//  Created by Andrea Bizzotto on 08/08/2016.
+//  Copyright © 2016 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AcceptanceMark.
+FOUNDATION_EXPORT double AcceptanceMarkVersionNumber;
+
+//! Project version string for AcceptanceMark.
+FOUNDATION_EXPORT const unsigned char AcceptanceMarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AcceptanceMark/PublicHeader.h>
+
+
